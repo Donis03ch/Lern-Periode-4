16705355aeffd920a3db2b1f06a11eda161c5b90
Donis03ch <111046453+Donis03ch@users.noreply.github.com>
2024-04-05 15:48:52 +0200

    Add files via upload

---
+++ HomePage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using iTextSharp.text.pdf.parser;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DEBreader
+{
+
+    /// <summary>
+    /// Interaktionslogik für HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Window
+    {
+
+        private MainWindow parent;
+
+        public HomePage(MainWindow parent)
+        {
+            InitializeComponent();
+            this.parent = parent;
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            parent.Show();
+            this.Close();
+        }
+
+        private void CollectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new CollectionPage());
+            MainFrame.NavigationService.RemoveBackEntry();
+        }
+
+        private void LibraryButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new LibraryPage());
+            MainFrame.NavigationService.RemoveBackEntry();
+        }
+
+    }
+}
---
+++ LibraryPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DEBreader
+{
+    /// <summary>
+    /// Interaktionslogik für LibraryPage.xaml
+    /// </summary>
+    public partial class LibraryPage : Page
+    {
+        public LibraryPage()
+        {
+            InitializeComponent();
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.NavigationService.RemoveBackEntry();
+
+        }
+
+        private void CollectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new CollectionPage());
+            MainFrame.NavigationService.RemoveBackEntry();
+        }
+
+        private void LibraryButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new LibraryPage());
+            MainFrame.NavigationService.RemoveBackEntry();
+        }
+    }
+}
